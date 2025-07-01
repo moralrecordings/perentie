ce022fded346196e065d06fad42b0bca51f65ce6
Scott Percival <code@moral.net.au>
2025-07-01 10:22:51 +0800

    scripts: Add HTTPS support to test webserver

--- scripts/webserver.py
+++ scripts/webserver.py
@@ -4,9 +4,13 @@
 # https://stackoverflow.com/a/21957017
 # https://gist.github.com/HaiyangXu/ec88cbdce3cdbac7b8d5
 
-from http.server import SimpleHTTPRequestHandler
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+import argparse
 import socketserver
 import sys
+import ssl
+import pathlib
+
 
 class Handler(SimpleHTTPRequestHandler):
     extensions_map = {
@@ -29,7 +33,20 @@ class Handler(SimpleHTTPRequestHandler):
         SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
-    port = int(sys.argv[1]) if len(sys.argv) > 1 else 8000
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description="Simple web server for serving SDL3 Emscripten builds with the SharedMemoryBuffer headers", epilog="Mobile Safari won't work unless the server is HTTPS. Generate a self-signed cert with:\n    openssl req -x509 -newkey rsa:4096 -out cert.pem -sha256 -days 3650 -nodes -subj \"/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=CommonNameOrHostname\"")
+    parser.add_argument("--port", type=int, help="Port to use, defaults to 8000 for HTTP and 8443 for HTTPS")
+    parser.add_argument("--cert", type=pathlib.Path, help="Self-signed certificate.")
+    args = parser.parse_args()
+
+    port = args.port if args.port else (8443 if args.cert else 8000)
     with socketserver.TCPServer(("0.0.0.0", port), Handler) as httpd:
-        print(f"Emscripten port served at http://localhost:{port}/perentie.html")
-        httpd.serve_forever()
+        if args.cert:
+            sslc = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+            sslc.load_cert_chain(str(args.cert), str(args.cert))
+            with sslc.wrap_socket(httpd.socket, server_side=True) as sock:
+                httpd.socket = sock
+                print(f"Emscripten port served at https://localhost:{port}/perentie.html")
+                httpd.serve_forever()
+        else:
+            print(f"Emscripten port served at http://localhost:{port}/perentie.html")
+            httpd.serve_forever()
